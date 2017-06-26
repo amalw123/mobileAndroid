0996ccdc05c46e367908b37cffe0d2b935849b54
Amal <wickramarathna.amal@gmail.com>
2017-06-27 03:17:36 +0530

    initial commit

---
+++ app/src/main/java/com/example/a4m4l/dailycollection/DatabaseHelper.java
@@ -0,0 +1,45 @@
+package com.example.a4m4l.dailycollection;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by 4m4l on 6/26/17.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+
+    public static final String DATABASE_NAME = "dailyCollection.db";
+    public static final String TABLE_NAME = "user";
+    public static final String COL_1 = "id";
+    public static final String COL_2 = "name";
+    public static final String COL_3 = "email";
+    public static final String COL_4 = "password";
+
+
+
+
+
+
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table" +TABLE_NAME+ "(ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, password TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS" + TABLE_NAME);
+        onCreate(db);
+    }
+
+
+}
