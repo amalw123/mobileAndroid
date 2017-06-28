6960199015ec6d80a75955f1076bdd0b91f08966
Amal <wickramarathna.amal@gmail.com>
2017-06-28 23:25:24 +0530

    UI design

--- app/src/main/java/com/example/a4m4l/dailycollection/MainActivity.java
+++ app/src/main/java/com/example/a4m4l/dailycollection/MainActivity.java
@@ -2,16 +2,38 @@ package com.example.a4m4l.dailycollection;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
+    EditText name, email, password;
+    Button btnAddUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDb = new DatabaseHelper(this);
 
+        name = (EditText) findViewById(R.id.etname);
+        email = (EditText) findViewById(R.id.etemail);
+        password = (EditText) findViewById(R.id.etpassword);
+        btnAddUser = (Button) findViewById(R.id.btnaddUser);
+
+    }
+
+    public void addData(){
+        btnAddUser.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        
+                    }
+                }
+
+        );
 
     }
 }
