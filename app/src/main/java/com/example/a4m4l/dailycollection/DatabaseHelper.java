7c66516c5c40d227868b4eec36e2289d4c505ecf
Amal <wickramarathna.amal@gmail.com>
2017-06-28 07:23:56 +0530

    insert data query

--- app/src/main/java/com/example/a4m4l/dailycollection/DatabaseHelper.java
+++ app/src/main/java/com/example/a4m4l/dailycollection/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.a4m4l.dailycollection;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -17,10 +18,10 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public static final String COL_3 = "email";
     public static final String COL_4 = "password";
 
-    
+
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
+
     }
 
     @Override
@@ -32,6 +33,23 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS" + TABLE_NAME);
         onCreate(db);
+    }
+    public boolean insertData(String name, String email, String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,name );
+        contentValues.put(COL_3,email );
+        contentValues.put(COL_4,password );
+        long result =  db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1 )
+
+            return false;
+        else
+
+            return true;
+
+
+
     }
 
 }
